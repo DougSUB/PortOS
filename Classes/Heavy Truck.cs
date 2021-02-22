fe5064b2ebcd50bd4408701d7021872c002aaca4
Doug Subosits <doug.subosits@gmail.com>
2021-02-22 14:43:15 -0500

    Add project files.

---
+++ Classes/Heavy Truck.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortOS.Classes
+{
+	class Heavy_Truck
+	{
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PortOS
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
